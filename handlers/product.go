32e9107b85bef2008e627bbc576ee605bc388e16
khalid imad <muhamadkhalid899@gmail.com>
2022-10-26 21:41:41 +0700

    update transaction

--- handlers/product.go
+++ handlers/product.go
@@ -17,176 +17,173 @@ import (
 var path_file = "http://localhost:5000/uploads/"
 
 type handlerProduct struct {
-  ProductRepository repositories.ProductRepository
+	ProductRepository repositories.ProductRepository
 }
 
 func HandlerProduct(ProductRepository repositories.ProductRepository) *handlerProduct {
-  return &handlerProduct{ProductRepository}
+	return &handlerProduct{ProductRepository}
 }
 
 func (h *handlerProduct) FindProducts(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-  products, err := h.ProductRepository.FindProducts()
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
-    json.NewEncoder(w).Encode(response)
-    return
-  }
+	products, err := h.ProductRepository.FindProducts()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
-  for i, p := range products {
-  products[i].Image = path_file + p.Image
-}
+	for i, p := range products {
+		products[i].Image = path_file + p.Image
+	}
 
-  w.WriteHeader(http.StatusOK)
-  response := dto.SuccessResult{Code: http.StatusOK, Data: products}
-  json.NewEncoder(w).Encode(response)
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: http.StatusOK, Data: products}
+	json.NewEncoder(w).Encode(response)
 }
 
 func (h *handlerProduct) GetProduct(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-  id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
-  var product models.Product
-  product, err := h.ProductRepository.GetProduct(id)
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
-    json.NewEncoder(w).Encode(response)
-    return
-  }
+	var product models.Product
+	product, err := h.ProductRepository.GetProduct(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
-  product.Image = path_file + product.Image
+	product.Image = path_file + product.Image
 
-  w.WriteHeader(http.StatusOK)
-  response := dto.SuccessResult{Code: http.StatusOK, Data: convertResponseProduct(product)}
-  json.NewEncoder(w).Encode(response)
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: http.StatusOK, Data: convertResponseProduct(product)}
+	json.NewEncoder(w).Encode(response)
 }
 
 func (h *handlerProduct) CreateProduct(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-  userInfo := r.Context().Value("userInfo").(jwt.MapClaims)
-  userId := int(userInfo["id"].(float64))
+	userInfo := r.Context().Value("userInfo").(jwt.MapClaims)
+	userId := int(userInfo["id"].(float64))
 
-  dataContex := r.Context().Value("dataFile")
-  filename := dataContex.(string)
+	dataContex := r.Context().Value("dataFile")
+	filename := dataContex.(string)
 
-  price, _ := strconv.Atoi(r.FormValue("price"))
-  request := productdto.ProductRequest{
-    Title : r.FormValue("title"),
-    Price : price,
-    Image : r.FormValue("image"),
-  }
+	price, _ := strconv.Atoi(r.FormValue("price"))
+	request := productdto.ProductRequest{
+		Title: r.FormValue("title"),
+		Price: price,
+		Image: r.FormValue("image"),
+	}
 
-  validation := validator.New()
-  err := validation.Struct(request)
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
-    json.NewEncoder(w).Encode(response)
-    return
-  }
+	validation := validator.New()
+	err := validation.Struct(request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
-  product := models.Product{
-    Title:  request.Title,
-    Price:  request.Price,
-    Image:  filename,
-    UserID: userId,
-  }
+	product := models.Product{
+		Title:  request.Title,
+		Price:  request.Price,
+		Image:  filename,
+		UserID: userId,
+	}
 
-  product, err = h.ProductRepository.CreateProduct(product)
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
-    json.NewEncoder(w).Encode(response)
-    return
-  }
+	product, err = h.ProductRepository.CreateProduct(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
-  product, _ = h.ProductRepository.GetProduct(product.ID)
+	product, _ = h.ProductRepository.GetProduct(product.ID)
 
-  w.WriteHeader(http.StatusOK)
-  response := dto.SuccessResult{Code: http.StatusOK, Data: product}
-  json.NewEncoder(w).Encode(response)
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: http.StatusOK, Data: product}
+	json.NewEncoder(w).Encode(response)
 }
 
 func convertResponseProduct(u models.Product) models.ProductResponse {
-  return models.ProductResponse{
-    Title:    u.Title,
-    Price:    u.Price,
-    Image:    u.Image,
-    UserID: u.UserID,
-    User:     u.User,
-    Category: u.Category,
-  }
+	return models.ProductResponse{
+		Title:  u.Title,
+		Price:  u.Price,
+		Image:  u.Image,
+		UserID: u.UserID,
+		User:   u.User,
+	}
 }
 
 func (h *handlerProduct) UpdateProduct(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-  request := new(productdto.ProductRequest)
-  if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-    w.WriteHeader(http.StatusBadRequest)
-    response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}  
-    json.NewEncoder(w).Encode(response)
-    return
-  }
+	request := new(productdto.ProductRequest)
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
-  id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
-  product, _ := h.ProductRepository.GetProduct(id)
+	product, _ := h.ProductRepository.GetProduct(id)
 
-  if request.Title != "" {
-    product.Title = request.Title
-  }
+	if request.Title != "" {
+		product.Title = request.Title
+	}
 
-  if request.Price != 0 {
-    product.Price = request.Price
-  }
+	if request.Price != 0 {
+		product.Price = request.Price
+	}
 
-  if request.Image != "" {
-    product.Image = request.Image
-  }
+	if request.Image != "" {
+		product.Image = request.Image
+	}
 
-  data, err := h.ProductRepository.UpdateProduct(product,id)
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()} 
-    json.NewEncoder(w).Encode(response)
-    return
-    }
+	data, err := h.ProductRepository.UpdateProduct(product, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
-  w.WriteHeader(http.StatusOK)
-  response := dto.SuccessResult{Code: http.StatusOK, Data:convertResponseProduct(data)}
-  json.NewEncoder(w).Encode(response)
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: http.StatusOK, Data: convertResponseProduct(data)}
+	json.NewEncoder(w).Encode(response)
 }
 
 func (h *handlerProduct) DeleteProduct(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-  id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
 
-  product, err := h.ProductRepository.GetProduct(id)
-  if err != nil {
-    w.WriteHeader(http.StatusBadRequest)
-    response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}  
-    json.NewEncoder(w).Encode(response)
-    return
-  }
+	product, err := h.ProductRepository.GetProduct(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
-  data, err := h.ProductRepository.DeleteProduct(product, id)
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()} 
-    json.NewEncoder(w).Encode(response)
-    return
-  }
+	data, err := h.ProductRepository.DeleteProduct(product, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
 
-  w.WriteHeader(http.StatusOK)
-  response := dto.SuccessResult{Code: http.StatusOK, Data: convertResponseProduct(data)}
-  json.NewEncoder(w).Encode(response)
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Code: http.StatusOK, Data: convertResponseProduct(data)}
+	json.NewEncoder(w).Encode(response)
 }
-
-
--- models/product.go
+++ models/product.go
@@ -3,41 +3,37 @@ package models
 import "time"
 
 type Product struct {
-  ID         int                  `json:"id"`
-  Title      string               `json:"title" form:"title" gorm:"type: varchar(255)"`
-  Price      int                  `json:"price" form:"price" gorm:"type: int"`
-  Image      string               `json:"image" form:"image" gorm:"type: varchar(255)"`
-  UserID     int                  `json:"user_id" form:"user_id"`    
-  User       UsersProfileResponse `json:"user"`
-  Category   []Category           `json:"category" gorm:"many2many:product_categories"`
-  CategoryID []int                `json:"category_id" form:"category_id" gorm:"-"`
-  CreatedAt  time.Time            `json:"-"`
-  UpdatedAt  time.Time            `json:"-"`
+	ID        int                  `json:"id"`
+	Title     string               `json:"title" form:"title" gorm:"type: varchar(255)"`
+	Price     int                  `json:"price" form:"price" gorm:"type: int"`
+	Image     string               `json:"image" form:"image" gorm:"type: varchar(255)"`
+	UserID    int                  `json:"user_id" form:"user_id"`
+	User      UsersProfileResponse `json:"user"`
+	CreatedAt time.Time            `json:"-"`
+	UpdatedAt time.Time            `json:"-"`
 }
 
 type ProductResponse struct {
-  ID         int                  `json:"id"`
-  Title      string               `json:"title"`
-  Price      int                  `json:"price"`
-  Image      string               `json:"image"`
-  UserID     int                  `json:"-"`
-  User       UsersProfileResponse `json:"user"`
-  Category   []Category           `json:"category" gorm:"many2many:product_categories"`
-  CategoryID []int                `json:"category_id" form:"category_id" gorm:"-"`
+	ID     int                  `json:"id"`
+	Title  string               `json:"title"`
+	Price  int                  `json:"price"`
+	Image  string               `json:"image"`
+	UserID int                  `json:"-"`
+	User   UsersProfileResponse `json:"user"`
 }
 
 type ProductUserResponse struct {
-  ID     int    `json:"id"`
-  Title   string `json:"title"`
-  Price  int    `json:"price"`
-  Image  string `json:"image"`
-  UserID int    `json:"-"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Price  int    `json:"price"`
+	Image  string `json:"image"`
+	UserID int    `json:"-"`
 }
 
 func (ProductResponse) TableName() string {
-  return "products"
+	return "products"
 }
 
 func (ProductUserResponse) TableName() string {
-  return "products"
+	return "products"
 }
